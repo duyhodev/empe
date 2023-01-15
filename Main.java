a779ac505fc4395bf96f7b0373b65a8d6ed86bfa
Duy Ho <duy.ho@pickupp.io>
2023-01-15 22:26:47 +0700

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    static int add(int a, int b) {
+        return 0;
+    }
+
+    static int substract(int a,int b) {
+        return 0;
+    }
+
+    public static void main(String[] argvs) {
+        System.out.println(add(1, 2));
+        System.out.println(substract(3, 4));
+    }
+}
